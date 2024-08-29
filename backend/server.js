bd4746acec9a78e03e19811f021ed1ac6a43a449
Tseekii <tseekiibatanu@gmail.com>
2024-08-29 13:03:37 +0800

    829

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,25 @@
-// const express = require("express");
-// const app = express();
-// const fs = require("fs"); //fs=file system
-const cors = require("cors");
-
 // app.use(express.json()); // middleware
 // app.use(cors()); //haanaas ch handaj bolno widdleware
-
-// app.get("/users", (req, res) => {
-//   console.log("users", req.body);
-// });
-
-// app.post("/users", (req, res) => {
-//   console.log("users", req.body);
-// });
-
-// app.delete("/users", (req, res) => {
-//   console.log(req.params);
-// });
-
-// app.put("/users/:userId", (req, res) => {
-//   console.log(req.params);
-// });
-
-// app.listen(8000, () => {
-//   console.log("Server is running at local host: 8000");
-// });
 const express = require("express");
-
 const dotenv = require("dotenv");
-dotenv.config(); //
+const cors = require("cors");
+const { logger } = require("./middlewares/logger");
+dotenv.config();
+
+const userRoutes = require("./routes/user-route");
+const categoryRoutes = require("./routes/category-route");
 
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(express.json());
 
-app.get("/user", (req, res) => {
-  console.log("all user is read successfully");
-});
-app.post("/user", (req, res) => {
-  console.log("New user is created successfully");
-});
-app.put("/user", (req, res) => {
-  console.log("user is updated successfully");
-});
-app.delete("/user", (req, res) => {
-  console.log("user is deleted successfully");
-});
+app.use(cors());
+app.use(express.json());
+app.use(logger());
+
+app.use("/users", userRoutes);
+app.use("/categories", categoryRoutes);
 
 app.listen(PORT, () => {
-  console.log(`сервер localhost:${PORT} дээр аслаа`);
+  console.log(`Сервер localhost:${PORT} дээр аслаа.`);
 });
